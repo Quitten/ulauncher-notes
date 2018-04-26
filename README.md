1a3cb06d820f06e3d71395a738af28f113094213
Barak Tawily <barakt@wix.com>
2018-04-26 20:36:21 +0300

    first and last commit?

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ulauncher-notes
-# ulauncher-notes
+Just a stupid simple notes manager.
+# Usage
+Commands:
+note <content> - to add a new note
+notes - to read notes
+notes d/del/delete - to delete note
+notes c/copy - to copy to clipboard
+
---
+++ main.py
@@ -0,0 +1,93 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
+import json, uuid, pyperclip
+
+class NotesExtension(Extension):
+    def __init__(self):
+        super(NotesExtension, self).__init__()
+        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+
+notesFilePath = '/home/barak/.notes'
+
+def saveNote(note):
+    note['id'] = str(uuid.uuid4())
+    f = open(notesFilePath, 'a')
+    f.write('%s\n' % json.dumps(note))
+    f.close()
+
+def deleteNote(deleteNote):
+    notes = getNotes()
+    f = open(notesFilePath, 'w')
+    for note in notes:
+        if note['id'] != deleteNote['id']:
+            f.write('%s\n' % json.dumps(note))
+    f.close()
+
+def copyToClipboard(note):
+    pyperclip.copy(note['data'])
+
+def getNotes():
+    notes = []
+    f = open(notesFilePath, 'r')
+    lines = f.read().split('\n')
+    f.close()
+
+    for data in lines:
+        if data == '' or data == None:
+            continue
+        note = json.loads(data)
+        notes.append(note)
+    return notes
+
+
+class KeywordQueryEventListener(EventListener):
+    def on_event(self, event, extension):
+        if event.get_keyword() == 'notes':
+            resNotes = []
+            f = open(notesFilePath, 'r')
+            lines = f.read().split('\n')
+            f.close()
+            
+            notes = getNotes()            	
+            for note in notes:
+                note['mode'] = ''
+                if event.get_argument() == 'delete' or event.get_argument() == 'del' or event.get_argument() == 'd':
+                    note['mode'] = 'deleteNote'
+                if event.get_argument() == 'copy' or event.get_argument() == 'c':
+                    note['mode'] = 'copyToClipboard'
+                resNotes.append(ExtensionResultItem(icon='note.png',
+                                         name=note['data'],
+                                         description='',
+                                         on_enter=ExtensionCustomAction(note, keep_app_open=True)))
+            return RenderResultListAction(resNotes)
+
+        if event.get_keyword() == 'note':
+            note = {}
+            note['data'] = event.get_argument()
+            note['mode'] = 'addNewNote'
+            return RenderResultListAction([ExtensionResultItem(icon='note.png',
+                                             name='New Note:',
+                                             description=note['data'],
+                                             on_enter=ExtensionCustomAction(note, keep_app_open=True))])
+
+
+class ItemEnterEventListener(EventListener):
+    def on_event(self, event, extension):
+        note = event.get_data()
+        if note['mode'] == 'addNewNote':
+            saveNote(note)
+        if note['mode'] == 'deleteNote':
+            deleteNote(note)
+        if note['mode'] == 'copyToClipboard':
+            copyToClipboard(note)
+
+        return HideWindowAction()
+
+if __name__ == '__main__':
+    NotesExtension().run()
